52fbbf5d773ef20de1e51c73fedaf780fca231da
S1SYPHOS <m.folkers@posteo.de>
2016-12-15 02:49:45 +0100

    Applied overhauled markup structure

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,50 @@
 <footer class="site-footer" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="footer-links one-half">
+      <?php
+      if (has_nav_menu('footer_navigation')) :
+        wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav']);
+      endif;
+      ?>
+    </div>
+    <div class="supporters one-half">
+      <div class="supporters_org mbl">
+        <p>Partner:</p>
+        <ul class="inline-list">
+          <li>
+            <a href="http://www.rapcircus.de" target="_blank">
+              <img src="/wp-content/themes/skillz/dist/images/rapcircus.png" height="45" alt="">
+            </a>
+          </li>
+          <li>
+            <a href="http://www.itsyours.info" target="_blank">
+              <img src="/wp-content/themes/skillz/dist/images/itsyours.png" height="45" alt="">
+            </a>
+          </li>
+          <li>
+            <a href="http://www.raputation.de" target="_blank">
+              <img src="/wp-content/themes/skillz/dist/images/raputation.png" height="45" alt="">
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="supporters_media">
+        <p>Medienpartner:</p>
+        <ul class="inline-list">
+          <li>
+            <a href="http://www.backspin.de/tag/skillz-awards/" target="_blank">
+              <img src="/wp-content/themes/skillz/dist/images/backspin.png" height="45" alt="">
+            </a>
+          </li>
+          <li>
+            <a href="http://www.radioblau.de" target="_blank">
+              <img src="/wp-content/themes/skillz/dist/images/radioblau.png" height="45" alt="">
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </footer>
-<script src="https://unpkg.com/isotope-layout@3.0/dist/isotope.pkgd.js"></script>
 
+<!-- <?php dynamic_sidebar('sidebar-footer'); ?> -->
--- templates/header.php
+++ templates/header.php
@@ -1,13 +1,27 @@
 <header class="site-header" role="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
+    <div class="logo">
+      <a class="brand" href="<?= esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>">
+        <img src="/wp-content/themes/skillz/dist/images/logo.png" alt="">
+      </a>
+    </div>
+    <div class="call-to-vote">
+      <a class="btn" href="<?php if( is_front_page() ) : ?>#content<?php else : ?>/voting/<?php endif; ?>" >zum Voting</a>
+    </div>
+    <nav class="nav-wrap nav-collapse">
+      <!-- <a class="nav-toggle"  href="#"><span></span></a> -->
+      <button class="nav-toggle hamburger hamburger--squeeze" type="button" data-nav-toggle="#nav-menu">
+        <span class="hamburger-box">
+          <span class="hamburger-inner"></span>
+        </span>
+      </button>
+      <div id="nav-menu" class="nav-menu">
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'header-nav inline-list']);
+        endif;
+        ?>
+      </div>
     </nav>
   </div>
 </header>
-
