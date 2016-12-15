8b1d5590fc56b159837cfb8fa626e6f00479da4f
S1SYPHOS <m.folkers@posteo.de>
2016-12-15 02:48:08 +0100

    Implemented basic styles & scripts, fonts & images

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,77 +1,50 @@
-/* ========================================================================
- * DOM-based Routing
- * Based on http://goo.gl/EUTi53 by Paul Irish
- *
- * Only fires on body classes that match. If a body class contains a dash,
- * replace the dash with an underscore when adding it to the object below.
- *
- * .noConflict()
- * The routing is enclosed within an anonymous function so that you can
- * always reference jQuery with $, even when in .noConflict() mode.
- * ======================================================================== */
+jQuery(function ($) {
 
-(function($) {
 
-  // Use this variable to set up the common and page specific functions. If you
-  // rename this variable, you will also need to rename the namespace below.
-  var Sage = {
-    // All pages
-    'common': {
-      init: function() {
-        // JavaScript to be fired on all pages
-      },
-      finalize: function() {
-        // JavaScript to be fired on all pages, after page specific JS is fired
-      }
-    },
-    // Home page
-    'home': {
-      init: function() {
-        // JavaScript to be fired on the home page
-      },
-      finalize: function() {
-        // JavaScript to be fired on the home page, after the init JS
-      }
-    },
-    // About us page, note the change from about-us to about_us.
-    'about_us': {
-      init: function() {
-        // JavaScript to be fired on the about us page
-      }
+// isotope init
+var $container = $('#isotope-list'); //The ID for the list with all the blog posts
+$container.isotope({ //Isotope options, 'item' matches the class in the PHP
+itemSelector : '.item',
+layoutMode : 'fitRows'
+});
+
+//Add the class selected to the item that is clicked, and remove from the others
+var $optionSets = $('#filters'),
+$optionLinks = $optionSets.find('a');
+
+$optionLinks.click(function(){
+var $this = $(this);
+// don't proceed if already selected
+if ( $this.hasClass('selected') ) {
+return false;
+}
+var $optionSet = $this.parents('#filters');
+$optionSets.find('.selected').removeClass('selected');
+$this.addClass('selected');
+
+//When an item is clicked, sort the items.
+var selector = $(this).attr('data-filter');
+$container.isotope({ filter: selector });
+
+return false;
+});
+
+
+
+// smooth scroll
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 750);
+      return false;
     }
-  };
+  }
+});
 
-  // The routing fires all common scripts, followed by the page specific scripts.
-  // Add additional events for more control over timing e.g. a finalize event
-  var UTIL = {
-    fire: function(func, funcname, args) {
-      var fire;
-      var namespace = Sage;
-      funcname = (funcname === undefined) ? 'init' : funcname;
-      fire = func !== '';
-      fire = fire && namespace[func];
-      fire = fire && typeof namespace[func][funcname] === 'function';
 
-      if (fire) {
-        namespace[func][funcname](args);
-      }
-    },
-    loadEvents: function() {
-      // Fire common init JS
-      UTIL.fire('common');
 
-      // Fire page-specific init JS, and then finalize JS
-      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
-        UTIL.fire(classnm);
-        UTIL.fire(classnm, 'finalize');
-      });
-
-      // Fire common finalize JS
-      UTIL.fire('common', 'finalize');
-    }
-  };
-
-  // Load Events
-  $(document).ready(UTIL.loadEvents);
-
-})(jQuery); // Fully reference jQuery after this point.
+});
